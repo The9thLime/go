53871ec33df912cbf06c663a61ceda9974494ce3
Ayush Jain <ayushj0909@outlook.com>
2024-03-26 22:08:57 +0530

    Implicit and Explicit Declaration and Default Values

--- go_tut1.go
+++ go_tut1.go
@@ -3,13 +3,32 @@ package main
 import "fmt"
 
 func main() {
+	// Explicitly assigning data types to variables
 	var (
-		name string  = "Hello world!"
-		num1 byte    = 254
+		name string  = "Hello world!" // Explicit declaration
+		num1 byte    = 254            // Byte is simply int8
 		num2 int32   = -234729424
 		num3 float32 = -424234.044
 		TorF bool    = true
 	)
 
+	// Print variables with explicit data types
 	fmt.Println(name, num1, num2, num3, TorF)
+
+	// Implicitly declaring variables using the 'Walrus operator' or short variable declaration.
+	// The compiler guesses the data type when variables are defined implicitly.
+	numGuess := -10525.68
+
+	fmt.Printf("%T\n", numGuess) // Returns float64
+	// numGuess = "Hello" will return an error cause the compiler has already assigned the variable a suitable data type
+
+	// Unassigned variables are assigned a default value
+
+	var (
+		str  string // Empty string ""
+		num4 uint64 // 0
+		bl   bool   // false
+	)
+	fmt.Println(str, num4, bl)
+
 }
