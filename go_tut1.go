531a6fc73c4a6a6b5e539c7444021fed77d73a1c
Ayush Jain <ayushj0909@outlook.com>
2024-03-26 21:31:38 +0530

    Data types

--- go_tut1.go
+++ go_tut1.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	var (
+		name string  = "Hello world!"
+		num1 byte    = 254
+		num2 int32   = -234729424
+		num3 float32 = -424234.044
+		TorF bool    = true
+	)
+
+	fmt.Println(name, num1, num2, num3, TorF)
 }
